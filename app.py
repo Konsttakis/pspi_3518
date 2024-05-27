3ee40fb005f91d7af5d8e6c718fa630b87efdeb2
Konstantinos <konsttakis@csd.auth.gr>
2024-05-27 18:21:05 +0300

    Added crawler

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # BEGIN CODE HERE
-from flask import Flask
+from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from pymongo import TEXT
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 # END CODE HERE
 
 app = Flask(__name__)
@@ -47,5 +52,32 @@ def content_based_filtering():
 @app.route("/crawler", methods=["GET"])
 def crawler():
     # BEGIN CODE HERE
-    return ""
+    
+    
+    semester = semester = request.args.get('semester')
+    url = "https://qa.auth.gr/el/x/studyguide/600000438/current"
+
+    options = Options()
+    options.headless = True
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    elementID = "exam" + str(semester)
+
+    exam_element = driver.find_element(By.ID, elementID)
+    tbody_element = exam_element.find_element(By.TAG_NAME, "tbody")
+
+    rows = tbody_element.find_elements(By.TAG_NAME, "tr")
+    course_titles = []
+    for row in rows:
+            course_title = row.get_attribute("coursetitle")
+            if course_title:
+                course_titles.append(course_title)
+            
+    driver.quit()
+
+    res = {"course_titles": course_titles}
+
+    return res
     # END CODE HERE
